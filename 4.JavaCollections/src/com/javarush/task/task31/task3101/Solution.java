688c1ab0a0760d442c0e330500fd5371661e4aa3
skorpion <savinov_vu@inbox.ru>
2017-10-14 08:48:40 +0800

    implement search file.

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task31.task3101;
 
-/* 
+import java.io.File;
+import java.util.Arrays;
+
+/*
 Проход по дереву файлов
 */
 public class Solution {
     public static void main(String[] args) {
+
+        File folder = new File(args[0]);
+        Arrays.stream(folder.listFiles()).map(File::getTotalSpace).forEach(System.out::println);
+
 
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/* 
+Находим все файлы
+*/
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        File[] array = new File(root).listFiles();
+        List<String> fileList = new ArrayList<>();
+        Deque<File> queue = new ArrayDeque<>();
+        for (File file : array) {
+            checkDirectory(queue, file);
+            checkFile(fileList, file);
+        }
+
+
+       while (queue.size() > 0){
+            File directory = queue.pop();
+            File[] files = directory.listFiles();
+            if (files.length == 0) continue;
+
+            for (File file : files) {
+                checkDirectory(queue, file);
+                checkFile(fileList, file);
+            }
+        }
+        return fileList;
+    }
+
+    private static void checkFile(List<String> fileList, File file) {
+        if (file.isFile()) {
+            fileList.add(file.getAbsolutePath());
+        }
+    }
+
+    private static void checkDirectory(Deque<File> deque, File file) {
+        if (file.isDirectory()) {
+            deque.push(file);
+//            Collections.addAll(deq, new File(file.getAbsolutePath()).listFiles());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            List<String> fileTree = getFileTree(args[0]);
+            fileTree.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
