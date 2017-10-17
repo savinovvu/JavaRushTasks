764e092bc5e431be7e7f9f1d9752e0f2dea500c1
skorpion <savinov_vu@inbox.ru>
2017-10-17 20:40:35 +0800

    обход дерева файлов с помощью Files.walkFileTree(path, simpleFileVisitor)

--- 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -3,16 +3,15 @@ package com.javarush.task.task31.task3113;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Что внутри папки?
 */
 public class Solution {
-
 
 
     public static void main(String[] args) throws IOException {
@@ -22,12 +21,34 @@ public class Solution {
             System.out.println(path + " - не папка");
             return;
         }
-        new SimpleFileVisitor<Path>(){
+        final List<Integer> foldersCounter = new ArrayList<>();
+        final List<Integer> filesCounter = new ArrayList<>();
+        final List<Integer> sizeCounter = new ArrayList<>();
+        sizeCounter.add(Integer.valueOf(0));
+
+        SimpleFileVisitor<Path> simpleFileVisitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                foldersCounter.add(1);
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                sizeCounter.add(sizeCounter.get(sizeCounter.size() - 1) + Files.readAllBytes(file).length);
+                filesCounter.add(1);
+
+                return super.visitFile(file, attrs);
+            }
 
         };
-//        Files.walkFileTree(path,);
+        Files.walkFileTree(path, simpleFileVisitor);
 
+        System.out.println("Всего папок - " + (foldersCounter.size()-1));
+        System.out.println("Всего файлов - " + filesCounter.size());
+        System.out.println("Общий размер - " + sizeCounter.get(sizeCounter.size() - 1));
 
 
     }
 }
+
