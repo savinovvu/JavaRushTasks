732363f81c8b6d0aaae9a9557a7c718f89595e45
skorpion <savinov_vu@inbox.ru>
2017-10-28 12:36:57 +0800

    MVC. рефакторинг для создания MVC архитектуры.

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/View.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task36.task3601;
+
+public class View {
+
+    public void fireEventShowData() {
+        System.out.println(new Controller().onDataListShow());
+    }
+
+
+}
