342907ede5748aa5bd86b54f13599e3d08649458
skorpion <savinov_vu@inbox.ru>
2017-10-28 21:57:32 +0800

    Работа с массивами.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -1,15 +1,39 @@
 package com.javarush.task.task07.task0706;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-/* 
+/*
 Улицы и дома
 */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        int[] homes = new int[15];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < homes.length; i++) {
+            homes[i] = Integer.valueOf(reader.readLine());
+        }
+        int add = 0;
+        int odd = 0;
+
+        for (int i = 0; i < homes.length; i++) {
+            if (i % 2 == 0) {
+                add += homes[i];
+            } else {
+                odd += homes[i];
+            }
+        }
+
+        if (add > odd) {
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        }else {
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
+
+        }
+
+
     }
 }
